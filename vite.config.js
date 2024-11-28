9b82f348c51bb425523948990efa7d2525d9f248
Rahulr-dev <rahulrnair880@gmail.com>
2024-11-29 01:39:17 +0530

    Updated script and build

--- vite.config.js
+++ vite.config.js
@@ -2,21 +2,22 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+  plugins: [vue()],
   build: {
+    outDir: 'dist', // Output directory
     lib: {
-      entry: './src/index.js',
+      entry: './src/index.js', // Entry file for the library
       name: 'DynamicBreadcrumb',
+      formats: ['es', 'cjs', 'umd'], // Supported formats
       fileName: (format) => `dynamic-breadcrumb.${format}.js`,
     },
     rollupOptions: {
-      external: ['vue', 'react'],
+      external: ['vue'], // Externalize Vue dependency
       output: {
         globals: {
           vue: 'Vue',
-          react: 'React',
         },
       },
     },
   },
-  plugins: [vue()],
 });
